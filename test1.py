69ae7ffed97eb6ddc45f478cf0acadaaea6c9214
grego10 <46965233+grego10@users.noreply.github.com>
2019-07-12 16:09:40 -0400

    Update test1.py
    ameliorations to the read and flowCount functions (merge, clarity..)

--- test1.py
+++ test1.py
@@ -4,21 +4,31 @@ Created on Sun Jun 16 14:44:46 2019
 
 @author: greg0
 """
-# Load journey data
-
+# Load journeys in a panda dataframe
 def read():
     f = 'OD_2019-05.csv'
     data = pd.read_csv(f)
-
+    
+    #parsing through date data
     data['start_date'] = pd.to_datetime(data['start_date'], format='%Y-%m-%d', errors='coerce')
+    data['end_date'] = pd.to_datetime(data['end_date'], format='%Y-%m-%d', errors='coerce')
+    
+    #adding a day number column to identify days of the week
     data['day_number'] = pd.to_datetime(data['start_date']).dt.dayofweek
+    
+    #adding a boolean week day column
     data['week_day_b'] = pd.Series( index=data.index)
-    data['week_day_b'] = 0
-    data.loc[data['day_number'].isin([5, 6]), 'week_day_b'] = 1
+    data['week_day_b'] = 1
+    data.loc[data['day_number'].isin([5, 6]), 'week_day_b'] = 0
+             
+    #adding a column for to split the departures in 20 min groups
     data['time_slice'] = data['start_date'].dt.hour * 60 + np.floor(data['start_date'].dt.minute/20)*20
+    pd.options.mode.chained_assignment = None
+    data['time_slice'] = data['time_slice'].astype('int64')
     
     return data
 
+#creates a bar chart with the departures per day
 def visualizePerDay(data, column_name, color='#494949', title='Departure per day'):
   
     plt.figure(figsize=(20, 10))
@@ -30,12 +40,13 @@ def visualizePerDay(data, column_name, color='#494949', title='Departure per day
     ax.set_title(title)
     plt.show()
     
-def visualizePerHourW(data, column_name, color='#494949', title='Departure per hour (Week)'):
+#creates a bar chart with the departures per hour during the weekend
+def visualizePerHourEnd(data, column_name, color='#494949', title='Departure per hour (Week)'):
     
-    #WEEK
-    dataWeek = data.drop(data[data['week_day_b'] == 1].index)
+    #WEEKEND
+    dataWeekend = data.drop(data[data['week_day_b'] == 1].index)
     plt.figure(figsize=(20, 10))
-    ax = (dataWeek[column_name].groupby(dataWeek[column_name])
+    ax = (dataWeekend[column_name].groupby(dataWeekend[column_name])
                          .count()).plot(kind="bar", color=color)
     ax.set_facecolor('#eeeeee')
     ax.set_xlabel("Hour of day")
@@ -45,7 +56,8 @@ def visualizePerHourW(data, column_name, color='#494949', title='Departure per h
     ax.set_title(title)
     plt.show()
 
-def visualizePerHourE(data, column_name, color='#494949', title='Departure per hour (Weekend)'):
+#creates a bar chart with the departures per hour during the week
+def visualizePerHourWeek(data, column_name, color='#494949', title='Departure per hour (Weekend)'):
     
     #WEEK
     dataWeek = data.drop(data[data['week_day_b'] == 0].index)
@@ -64,18 +76,7 @@ def foliumMap():
     import folium
     from folium.plugins import MarkerCluster
 
-    # Change colours
-
-    def color_change(c):
-        if(c < 15):
-            return('red')
-        elif(15 <= c < 30):
-            return('orange')
-        else:
-            return('green')
-    
     # Create base map
-    
     Montreal = [45.508154, -73.587450]
     map = folium.Map(location = Montreal,
                      zoom_start = 12, 
@@ -89,40 +90,46 @@ def foliumMap():
     
     marker_cluster = MarkerCluster(locations=[lat, lon]).add_to(map)        
     
-    # Plot markers
-       
+    # Plot markers for stations
     for _lat, _lon, _name in zip(lat, lon, name):
         folium.CircleMarker(location = [_lat, _lon], 
                             radius = 9, 
-                            popup = _name, 
-#                            fill_color = color_change(_cap), 
+                            popup = _name,
                             color = "gray", 
                             fill_opacity = 0.9).add_to(marker_cluster)
         
     f = 'map_station_cluster.html'
     map.save(f)
     
-def flowCount(data):
+def flowCount(data, start_time, end_time):
 
     file_stations = 'Stations_2019.csv'
     stations = pd.read_csv(file_stations)
-    lat = stations['latitude'].values
-    lon = stations['longitude'].values
-    name = stations['name'].values
     
-    #select weekday
-    data = data.drop(data[data['week_day_b'] == 1].index)
-    #select morning
-    data = data.drop(data[data['time_slice'].between(0,840)].index)
-    print(data)    
+    #drop weekends
+    data = data.drop(data[data['week_day_b'] == 0].index)
+    
+    #select time slot
+    data = data.drop(data[data['time_slice'].between(0,start_time)].index)
+    data = data.drop(data[data['time_slice'].between(end_time, 1420)].index)
+    
+    #agregate the number of departures per stations
     data_s = data.groupby('start_station_code').size().to_frame('departures_cnt').reset_index()
-    data_e = data.groupby('end_station_code').size().to_frame('arrivals_cnt').reset_index()
-  
-    stations['departures'] = data_s['departures_cnt']
-    stations['arrivals'] = data_e['arrivals_cnt']
+    data_s = data_s.rename(columns={'start_station_code':'Code'})
+    data_e = data.groupby('end_station_code').size().to_frame('arrivals_cnt').reset_index()  
+    data_e = data_e.rename(columns={'end_station_code':'Code'})
+    
+    #add a net departur column
+    stations = pd.merge(stations, data_s, on='Code')
+    stations = pd.merge(stations, data_e, on='Code')
+    stations['net_departures'] = pd.Series( index=data.index)
+    stations['net_departures'] = stations['departures_cnt'] - stations['arrivals_cnt']
+    
+    #replace stations with 0 net_departures by 1 to avoid calcultions using 0
+    stations.loc[stations['net_departures'].eq(0), 'net_departures'] = 1
+    
     return stations
     
-
 def densityMap(stations):
 
     Montreal = [45.508154, -73.587450]
@@ -130,8 +137,6 @@ def densityMap(stations):
                 zoom_start = 12,
                 tiles = "CartoDB dark_matter")
     
-    stations['net_departures'] = pd.Series( index=data.index)
-    stations['net_departures'] = stations['departures'] - stations['arrivals']
     stations['radius'] = pd.Series( index=data.index)
     
     stations['radius'] = np.abs(stations['net_departures'])
@@ -170,12 +175,12 @@ if __name__ == '__main__':
     
     data = read()
     
-    stations = flowCount(data)
+    stations_flow = flowCount(data, 300, 660)
     densityMap(stations)
-    #foliumMap()
-    #visualizePerDay(data, 'start_date')
-    #visualizePerHourW(data, 'time_slice')
-    #visualizePerHourE(data, 'time_slice')
+    foliumMap()
+    visualizePerDay(data, 'start_date')
+    visualizePerHourWeek(data, 'time_slice')
+    visualizePerHourEnd(data, 'time_slice')
     
 
 
